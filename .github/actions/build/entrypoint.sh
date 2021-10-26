25a045924a90d138c3efaed9e6411cd29909c119
Jake Mulford <jakemulf@gmail.com>
2021-10-26 14:49:22 -0400

    ripped out build into a separate action

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -1,29 +1,4 @@
 #!/bin/sh
 
-set -o errexit
-set -o pipefail
-set -o nounset
-
 $2
-
-docker-compose -f $1 up -d
-
-while true; do
-    STATUS="$(docker inspect --format {{.State.Health.Status}} workspace_grpc-api_1)"
-    echo "Status: '${STATUS}'"
-    if [ "${STATUS}" != "starting" ]; then
-        break
-    fi
-    sleep 1
-done
-
-STATUS="$(docker inspect --format {{.State.Health.Status}} workspace_grpc-api_1)"
-echo "Final status: '${STATUS}'"
-
-#docker-compose -f $1 down -v
-
-#if [ "${STATUS}" != "healthy" ]; then
-#    exit 1
-#fi
-
-exit 0
+docker-compose -f $1 -p $3 up -d
--- .github/actions/validate-tables/entrypoint.sh
+++ .github/actions/validate-tables/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
 docker build --tag deephaven-examples/validate-tables /github-actions-validate/
-$4
-
-docker-compose -f $3 -p $5 up -d
-TABLE_NAMES=$1 HOST=$2 docker-compose -f /github-actions-validate/docker-compose.yml -p $5 up --exit-code-from validate-tables
+TABLE_NAMES=$1 HOST=$2 docker-compose -f /github-actions-validate/docker-compose.yml -p $3 up --exit-code-from validate-tables
