962e59f742ffe3d91926dce4fbe714a578eaa9d0
Jake Mulford <jakemulf@gmail.com>
2021-10-26 21:17:14 -0400

    testing list args

--- .github/actions/validate-tables/entrypoint.sh
+++ .github/actions/validate-tables/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+echo $1
+
 docker build --tag deephaven-examples/validate-tables /github-actions-validate/
 TABLE_NAMES=$1 HOST=$2 docker-compose -f /github-actions-validate/docker-compose.yml -p $3 up --exit-code-from validate-tables
